07cab0e080d5bef0bdd68cb094a67e92dac7c57f
Demaris Ashmore <demiashmore@yahoo.com>
2022-09-08 16:09:09 -0600

    commit tree 04d9f2b2b812f274aa2b8c5cc7a80afaf263158a

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // const linkElements = links.map((link) => {
+  //   return <a key={link} href={link}>{link}</a>
+  // })
+
+  return <nav>
+    {/* {linkElements} */}
+    <a key={links[0]} href='#home'>{links[0]}</a>
+    <a key={links[1]} href='#about'>{links[1]}</a>
+    <a key={links[2]} href='#projects'>{links[2]}</a>
+  </nav>;
 }
 
 export default NavBar;
